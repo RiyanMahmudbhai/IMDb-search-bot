c7b6bb6c658223863837cdc8746f0a973bc8c095
Riyan Mahmud <118922773+RiyanMahmudbhai@users.noreply.github.com>
2025-03-04 16:39:29 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,21 @@
+from pyrogram import filters
 
+class Config:
+    BOT_TOKEN = "YOUR_BOT_TOKEN"
+    API_ID = "YOUR_API_ID"
+    API_HASH = "YOUR_API_HASH"
+    IMDB_TEMPLATE = """
+🏷 <b>Title</b>: <b>{title}</b> [{year}]
+🎭 <b>Genres</b>: {genres}
+🌟 <b>Rating</b>: {rating} (based on {votes} user ratings)
+☀️ <b>Languages</b>: {languages}
+📀 <b>Runtime</b>: {runtime}
+📆 <b>Release Info</b>: {release_date}
+🎛 <b>Countries</b>: {countries}
+
+📖 <b>Storyline</b>:
+<i>{plot}</i>
+    """
+    # Add other configurations as needed
+
+config_dict = Config()
